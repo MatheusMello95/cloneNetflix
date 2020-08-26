4e958de70047e7d292ecd74f3b63060569c6cd20
Matheus Mello <matheusamellom95@gmail.com>
2020-08-26 14:53:26 -0300

    Updates

---
+++ precache-manifest.499d4e4647667847a596f0b9a571b973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f3f68a903cc94327daf951c14971bb",
+    "url": "/cloneNetflix/index.html"
+  },
+  {
+    "revision": "6380d51319988c73d193",
+    "url": "/cloneNetflix/static/css/main.b55fa0c4.chunk.css"
+  },
+  {
+    "revision": "761e083ccce44a0c3833",
+    "url": "/cloneNetflix/static/js/2.b455cbb5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cloneNetflix/static/js/2.b455cbb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6380d51319988c73d193",
+    "url": "/cloneNetflix/static/js/main.f9d7365c.chunk.js"
+  },
+  {
+    "revision": "7264c576c4b75716b1b5",
+    "url": "/cloneNetflix/static/js/runtime-main.2cac5729.js"
+  }
+]);
